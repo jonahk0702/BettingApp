d3de7484056a825364fd73dd4de72a056276b834
Dan Rom <DanRom@Macintoshs-MacBook-Air.local>
2019-04-30 23:37:30 +0200

    Change

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,29 +1,35 @@
-import React from 'react';
-import { Nav, NavDropdown, Form,Button, Dropdown, ButtonToolbar} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import {
+  Nav,
+  NavDropdown,
+  Form,
+  Button,
+  Dropdown,
+  ButtonToolbar
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css";
 
-  const Navbar = () => {
-    return(
+const Navbar = () => {
+  return (
+    <div className="center">
+      <br />
+      <br />
 
-     
-        <div className='center'>
-        <br/><br/>
-          
-          <ButtonToolbar>
-              <Button variant="primary">Primary</Button>
-              <Button variant="secondary">Secondary</Button>
-              <Button variant="success">Success</Button>
-              <Button variant="warning">Warning</Button>
-              <Button variant="danger">Danger</Button>
-              <Button variant="info">Info</Button>
-              <Button variant="light">Light</Button>
-              <Button variant="dark">Dark</Button>
-              <Button variant="link">Link</Button>
-          </ButtonToolbar>   
-          
-        <h1 className='tc pa6'> Welcome to custom Betts! </h1>
-      </div>
+      <ButtonToolbar>
+        <Button variant="primary">Primary</Button>
+        <Button variant="secondary">Secondary</Button>
+        <Button variant="success">Success</Button>
+        <Button variant="warning">Warning</Button>
+        <Button variant="danger">Danger</Button>
+        <Button variant="info">Info</Button>
+        <Button variant="light">Light</Button>
+        <Button variant="dark">Dark</Button>
+        <Button variant="link">Link</Button>
+      </ButtonToolbar>
+
+      <h1 className="tc pa6"> Welcome to Custom Bets! </h1>
+    </div>
   );
-}
+};
 
 export default Navbar;
