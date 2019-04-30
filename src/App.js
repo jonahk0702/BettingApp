473785babc3433c6fce71ccaeab7404cdb625707
jonahk0702 <jonahkollenberg1@gmail.com>
2019-04-30 16:22:45 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import SignIn from './components/SignIn';
+import NavBar from './components/navbar/NavBar';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <div>
+
+    <NavBar/>
+    <SignIn/>
+    
+    </div>
+    
+  );
+}
+
+export default App;
