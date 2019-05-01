ea3edcc3306fe3abdb38365064e5e1fcdda2369a
Jonah kollenberg <jonahkollenberg@Jonahs-MacBook-Pro.local>
2019-05-01 20:30:14 +0200

    not adding anything. Just error

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import React from 'react';
-import './App.css';
-import SignIn from './components/SignIn/SignIn';
-import NavBar from './components/navbar/NavBar';
-import 'bootstrap/dist/css/bootstrap.css';
-import Particles from 'react-particles-js';
+import React, { Component } from "react";
+import "./App.css";
+import SignInOrReg from "./SignInOrReg";
+import NavBar from "./components/navbar/NavBar";
+import "bootstrap/dist/css/bootstrap.css";
+import Particles from "react-particles-js";
 
+class App extends Component {
+  state = { isLogin: true };
 
+  handleSwitch = () => {
+    this.state.isLogin === true
+      ? this.setState({ isLogin: false })
+      : this.setState({ isLogin: true });
+  };
 
-const backgroundOptions = {
-            		particles: {
-            			number: {
-            				value:150,
-            				density: {
-            					enable:true, 
-            					value_area:800
-            				}
-            			}
-            		}
-            	}
+  backgroundOptions = {
+    particles: {
+      number: {
+        value: 150,
+        density: {
+          enable: true,
+          value_area: 800
+        }
+      }
+    }
+  };
 
-function App() {
-  return (
-    <div>
-    	
-<NavBar/>
-    <SignIn/>
-
-
-    	<Particles className='particles'
-              params={backgroundOptions}
-              
-            />
-
-    
-    
-    </div>
-    
-  );
+  render() {
+    return (
+      <div>
+        <NavBar onSwitch={this.handleSwitch} page={this.state.isLogin} />
+        <SignInOrReg onSwitch={this.handleSwitch} page={this.state.isLogin} />
+        <Particles className="particles" params={this.backgroundOptions} />
+      </div>
+    );
+  }
 }
 
 export default App;
