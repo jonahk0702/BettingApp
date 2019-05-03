2d29b9e618b3928b5875e3b777aed557216b2c3c
Jonah kollenberg <jonahkollenberg@Jonahs-MacBook-Pro.local>
2019-05-03 13:18:48 +0200

    proper reg valdiation

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -28,29 +28,35 @@ class Register extends Component {
     this.setState({ country: val });
   }
 
-Validate = () => {
-  
-    let {email, password, firstName, secondName, dateOfBirth, passwordConfirm
-      , IdNumber} = this.state;
-      let {Country} = this.state;
-      console.log(email, "-", password, "-", firstName, "-", secondName, "-",
-                  dateOfBirth, "-", passwordConfirm, "-", Country, "-", IdNumber);
+ValidateFirst = () => {
+  let {firstName, secondName, dateOfBirth} = this.state;
 
-  //First, lets make sure all fields are filled in
-    if(email === ' ' || password === ' ' || firstName === ' ' || 
-      secondName === ' ' || dateOfBirth === ' ' || passwordConfirm === ' ' 
-      || /*country === ' ' || */IdNumber === ' '){
+  if(firstName === ' ' || secondName === ' ' || dateOfBirth === ' '){
+      console.log("Please fill in all infomation");
+    }else{
+          console.log("Sucess!");
+          this.setState({currentPage:2});
+        }
+        
+
+}
+
+ValidateFinal = () => {
+  let {Country, email, password, passwordConfirm, IdNumber} = this.state;
+  
+  if(email === ' ' || password === ' '
+      || passwordConfirm === ' ' || IdNumber === ' '
+      || Country === ' '){
       console.log("Please fill in all infomation");
     }
 
-    //if they are all completed, lets make sure email is valid
-    else if(!email.includes('.')) {
+   else if(!email.includes('.')) {
               if(!email.includes('@')){
                  console.log("Not a valid Email");
                }
         }
-        //Password length
-      else if (password.length < 8){
+
+        else if (password.length < 8){
           console.log("Password is too short");
         }
         //password contains special cases
@@ -66,14 +72,15 @@ Validate = () => {
         else if(password !== passwordConfirm){
           console.log("make sure the passwords match");
 
-        }
-        else{
+        }else{
           console.log("Sucess!");
         }
         
+}    
+        
      
     
-  }
+
 enterEmail = (newEmail) =>{
       this.setState({email: newEmail.target.value});
 
@@ -124,50 +131,50 @@ enterEmail = (newEmail) =>{
     //create account then show this part of the form
   
    
-    
+    let first;
+    let final;
 
 
-    return (
-
-      //Depending on page, im changing current, but whatever it is
-      //current is always displayed
-      <div>
-          <div className="mw5 mw7-ns center br5 black">
-              <br />
-              <br />
-
-              <article className="br3 ba --black-10 w-100 w-100-m center shadow-5 solidBR">
-                <main className="pa4 black-80 tc">
-                  <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-                    <legend className="f1 ph0 mh0">Register</legend>
-                    <p
-                      onClick={() => this.props.handleSwitch(false)}
-                      className="f6 link dim black db pointer tc">
-                      Already have an account?
-                    </p>
-                   <div className="mt3">
+    if(this.state.currentPage === 1){
+      first = <div><div className="mt3">
                         <label className="db fw6 lh-copy f6">First Name</label>
                         <input 
                         onChange={this.enterName}
                         className="pa2 input-reset ba bg-transparent hover-bg-black w-100" />
-                    </div>
-                    <div className="mv3">
+              </div>
+              <div className="mv3">
                         <label className="db fw6 lh-copy f6">Surname</label>
                         <input 
                         onChange={this.enterSurname}
-                        className="pa2 input-reset ba bg-transparent hover-bg-black w-100" />
-                    </div>
+                        className="pa2 input-reset ba bg-transparent hover-bg-black w-100" 
+                        />
+              </div>
                   
-                    <div className="mt3">
+                <div className="mt3">
                       <label className="db fw6 lh-copy f6">Date of Birth</label>
                       <input
                             onChange={this.enterDateofBirth}
                             className="pa2 input-reset ba bg-transparent hover-bg-black w-100"
                             type="date"
                       />
-                    </div>
+                </div>
 
-                    <div className="mt3">
+                    <br/>
+                    <input
+                          onClick={this.ValidateFirst}
+                          className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                          type="submit"
+                          value="Next"
+                        />
+            </div>
+
+            final = <div></div>
+}else{
+
+  first= <div></div>
+            final = <div>
+              
+                  <div className="mt3">
                       <label className="db fw6 lh-copy f6">email Adress</label>
                       <input 
                           onChange={this.enterEmail} className="pa2 input-reset ba bg-transparent hover-bg-black w-100" 
@@ -197,18 +204,51 @@ enterEmail = (newEmail) =>{
                       <div className="mt3">
                           <label className="db fw6 lh-copy f6">{country} Idenitifaction Number</label>
                           <input 
+                          type=''
                           onChange={this.enterIDNum}
                           className="pa2 input-reset ba bg-transparent hover-bg-black w-100" />
                       </div>
 
                       <div className="mt3">
                         <input
-                          onClick={this.Validate}
+                          onClick={this.ValidateFinal}
                           className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                           type="submit"
                           value="Create Account!"
                         />
                       </div>
+
+
+
+            </div>
+}
+    return (
+
+
+      //Depending on page, im changing current, but whatever it is
+      //current is always displayed
+      <div>
+          <div className="mw5 mw7-ns center br5 black">
+              <br />
+              <br />
+
+              <article className="br3 ba --black-10 w-100 w-100-m center shadow-5 solidBR">
+                <main className="pa4 black-80 tc">
+                  <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                    <legend className="f1 ph0 mh0">Register</legend>
+                    <p
+                      onClick={() => this.props.handleSwitch(false)}
+                      className="f6 link dim black db pointer tc">
+                      Already have an account?
+                    </p>
+                   
+
+
+                    {first} 
+
+                    {final}
+
+                    
                       <br />
                           
                     </fieldset>
